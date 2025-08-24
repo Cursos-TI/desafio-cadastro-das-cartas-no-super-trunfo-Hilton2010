65fca4160cbfc3807804cdbf75392968fc7204ce
Hilton2010 <hiltonsilvaneto@outlook.com>
2025-08-24 23:14:37 +0000

    Nova alteracao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,13 +8,13 @@
 
 int main() {
   int pop1, tur1, pop2, tur2;
-  char estado1, codigo_carta1, cidade1, estado2, codigo_carta2, cidade2;
+  char estado_1[1], codigo_carta1[3], cidade1[20], estado2[1], codigo_carta2[3], cidade2[20];
   float area1, pib1, area2, pib2;
 
   
   // Inclui dados da Carta 1
   printf("Digite o Estado da Carta 1: ");
-  scanf("%c", &estado1);
+  scanf("%s", &estado_1);
 
   printf("\n Digite o Código da Carta 1: ");
   scanf("%s", &codigo_carta1);
@@ -35,8 +35,8 @@ int main() {
   scanf("%d", &tur1);
 
   // Inclui dados da Carta 2
-  printf("Digite o Estado da Carta 2: \n");
-  scanf("%c", &estado2);
+  printf("\n Digite o Estado da Carta 2: ");
+  scanf("%s", &estado2);
 
   printf("\n Digite o Código da Carta 2: ");
   scanf("%s", &codigo_carta2);
@@ -55,10 +55,10 @@ int main() {
 
   printf("\n Digite o nº pontos turísticos da Cidade 2: ");
   scanf("%d", &tur2);
-
+ 
   // Mostra os dados da carta 1
   printf("Carta 1: \n");
-  printf("Estado: %c \n", estado1);
+  printf("Estado: %s \n", estado_1);
   printf("Código: %s \n", codigo_carta1);
   printf("Nome da Cidade: %s \n", cidade1);
   printf("População: %d \n", pop1);
@@ -68,14 +68,14 @@ int main() {
   printf(" \n");
 
 // Mostra os dados da carta 2
-  printf("Carta 2: \n");
-  printf("Estado: %c \n", estado2);
-  printf("Código: %s \n", codigo_carta2);
-  printf("Nome da Cidade: %s \n", cidade2);
-  printf("População: %d \n", pop2);
-  printf("Área: %.2f Km2 \n", area2);
-  printf("PIB: %.2f bilhões de reais \n", pib2);
-  printf("Nº de pontos turísticos: %d \n", tur2);
+   printf("Carta 2: \n");
+   printf("Estado: %s \n", estado2);
+   printf("Código: %s \n", codigo_carta2);
+   printf("Nome da Cidade: %s \n", cidade2);
+   printf("População: %d \n", pop2);
+   printf("Área: %.2f Km2 \n", area2);
+   printf("PIB: %.2f bilhões de reais \n", pib2);
+   printf("Nº de pontos turísticos: %d \n", tur2);
  
   return 0;
 }
